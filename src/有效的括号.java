93e2fca453e54ca20d5b9b7177d4b24cc05f87df
Hogan Zhang <george.zhz123@gmail.com>
2022-06-29 13:34:12 +0800

    Update 有效的括号.java
    update，

--- src/有效的括号.java
+++ src/有效的括号.java
@@ -3,14 +3,13 @@ import java.util.Map;
 import java.util.Stack;
 
 public class 有效的括号 {
-    public boolean isValid(String s){
-        Map<Character, Character> pairs = new HashMap<Character, Character>() {{
-            put(')', '(');
-            put(']', '[');
-            put('}', '{');
-        }};
+    public static boolean isValid(String s){
+        Map<Character, Character> pairs = new HashMap<>();
+        pairs.put(')','(');
+        pairs.put(']','[');
+        pairs.put('}','{');
 
-        Stack<Character> stack = new Stack<Character>(); // 栈中就只会放左括号
+        Stack<Character> stack = new Stack<>(); // 栈中就只会放左括号
         for (char ch:s.toCharArray()) {  // 把string 转换为 char
             if (pairs.containsKey(ch)){   // 如果碰到右括号, 就出栈，栈顶元素应该是hashmap的值
                 if (stack.isEmpty() || stack.peek() != pairs.get(ch)){
@@ -23,5 +22,9 @@ public class 有效的括号 {
             }
         }
         return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(有效的括号.isValid("{[()]}"));
     }
 }

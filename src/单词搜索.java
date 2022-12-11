cc412e622d6bc7a054fb53f29c7b342b74dc9bca
Hogan <hsbzzhz@126.com>
2022-12-11 22:03:07 +0800

    单词搜索 回溯

---
+++ src/单词搜索.java
@@ -0,0 +1,15 @@
+public class 单词搜索 {
+    /**
+     *
+     *
+     */
+
+    public static boolean exist(char[][] board, String word) {
+
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        exist(board, "ABCCED");
+    }
+}

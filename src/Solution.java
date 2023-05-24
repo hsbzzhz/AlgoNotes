caddbd67c02ac036722c21a5e8bd4269b8f3840d
Hogan <hsbzzhz@126.com>
2023-05-24 23:32:52 +0800

    add link

--- src/Solution.java
+++ src/Solution.java
@@ -2,36 +2,25 @@ import 基础数据结构操作.ListNode;
 import 基础数据结构操作.TreeNode;
 import java.util.*;
 public class Solution {
-    List<String> res = new ArrayList<>();
-    public List<String> generateParenthesis(int n) {
-        StringBuilder path = new StringBuilder();
-        dfs(n, n, path);
+    int res = 0;
+    public int getImportance(List<Employee> employees, int id) {
+        Employee currentEmp = employees.get(id);
+        if (currentEmp.subordinates.isEmpty()) {
+            return res;
+        }
+        res += currentEmp.importance;
+        for (Integer subId: currentEmp.subordinates) {
+            getImportance(employees, subId);
+        }
         return res;
     }
 
-    private void dfs(int left, int right, StringBuilder path) {
-        if (left < 0 || right < 0) {
-            return;
-        }
-        // 只有左边括号先于右边括号用完才行，秒啊
-        if (left > right) {
-            return;
-        }
-
-        if (left == 0 && right == 0) {
-            res.add(path.toString());
-            System.out.println(path);
-        }
-        path.append('(');
-        dfs(left-1, right, path);
-        path.deleteCharAt(path.length()-1);
-
-        path.append(')');
-        dfs(left, right-1, path);
-        path.deleteCharAt(path.length()-1);
-    }
-
     public static void main(String[] args) {
-        new Solution().generateParenthesis(3);
+        new Solution();
     }
 }
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
